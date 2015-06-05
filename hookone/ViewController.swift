d7667bfa8009f34ee3d06af64a4250b3233bbd1b
KARTHIK <dilipkumar@Dilips-Mac.local>
2015-06-05 03:07:25 -0700

    Initial Commit

---
+++ hookone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hookone
+//
+//  Created by KARTHIK on 6/5/15.
+//  Copyright (c) 2015 Sweatha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
